802c265015f6ed1f1cdf45894ff546d89a1c984a
batreller <batreller@gmail.com>
2024-11-06 13:35:46 +0000

    add font & change image

--- app/src/main/java/com/github/sviatoslavslysh/vacationvibes/api/ApiClient.java
+++ app/src/main/java/com/github/sviatoslavslysh/vacationvibes/api/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.1.19";
+    private static final String BASE_URL = "http://172.20.10.3";
     private static Retrofit retrofit;
 
     public static Retrofit getRetrofitInstance() {
